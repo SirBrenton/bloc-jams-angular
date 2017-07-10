3c50e6975a74f50bf4bd3dc1fdee0b216a47ec55
Brent Williams <brenton_williams@mac.com>
2017-07-10 11:50:16 -0700

    checkpoint 5 partial complete

---
+++ app/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,9 @@
+// define a controller for the Landing view
+(function() {
+     function LandingCtrl() {
+     }
+
+     angular
+         .module('blocJams')
+         .controller('LandingCtrl', LandingCtrl);
+ })();
